b427c5f4be78f7a7954d0e2ee13b2ba3e32f0722
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2025-12-31 10:37:37 +0100

    db config

---
+++ backend/config/database.php
@@ -0,0 +1,21 @@
+<?php
+    class Database {
+        private $host = "localhost";
+        private $dbname = "BuyMatch";
+        private $user = "vito";
+        private $pass = "vito123456789";
+        
+        public function connect() {
+            try{
+                return new PDO(
+                    "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4",
+                    $this->user,
+                    $this->pass,
+                    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+                );
+            } catch (PDOException $e) {
+                die("DB Error: ". $e->getMessage());
+            }   
+        } 
+    }
+?>
